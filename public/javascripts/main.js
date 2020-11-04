727968d97b9bd256a252313e1668e6689c08a39f
GustavMaki <gustav.siljamaki@gmail.com>
2020-11-04 12:17:24 +0100

    array test

---
+++ public/javascripts/main.js
@@ -0,0 +1,5 @@
+var notes = [{"id": "0", "title": "Titel", "content": "Content"}];
+let note = {"id": "1", "title": "Titel1", "content": "Content1"};
+notes.push(note);
+
+console.log (notes[0].title + notes[0].content);
